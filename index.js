51fdd646cb3c767991e3109b19b4d26e7b31690d
Rodrigo Vilar <rvilar@gmail.com>
2018-12-28 00:34:31 -0300

    Register, confirm and Login working

--- index.js
+++ index.js
@@ -1,88 +1,101 @@
+// This code is based on https://docs.aws.amazon.com/cognito/latest/developerguide/tutorial-integrating-user-pools-javascript.html#tutorial-integrating-user-pools-confirm-users-javascript
+
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
-const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
-const AWS = require('aws-sdk');
-const request = require('request');
-const jwkToPem = require('jwk-to-pem');
-const jwt = require('jsonwebtoken');
 global.fetch = require('node-fetch');
 
-const poolData = {    
-    UserPoolId : "", // Your user pool id here    
-    ClientId : "" // Your client id here
-    }; 
-const pool_region = 'us-east-1';
+var config = require('./config.json');
 
-const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
-function Login() {
-    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-        Username : 'sampleEmail@gmail.com',
-        Password : 'SamplePassword123',
-    });
-
-    var userData = {
-        Username : 'sampleEmail@gmail.com',
-        Pool : userPool
-    };
-    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    cognitoUser.authenticateUser(authenticationDetails, {
-        onSuccess: function (result) {
-            console.log('access token + ' + result.getAccessToken().getJwtToken());
-            console.log('id token + ' + result.getIdToken().getJwtToken());
-            console.log('refresh token + ' + result.getRefreshToken().getToken());
-        },
-        onFailure: function(err) {
-            console.log(err);
-        },
-
-    });
+function createCognitoUserAttribute(attributeList, name, value) {
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: name, Value: value})); 
 }
 
-function ValidateToken(token) {
-    request({
-        url: `https://cognito-idp.${pool_region}.amazonaws.com/${poolData.UserPoolId}/.well-known/jwks.json`,
-        json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            pems = {};
-            var keys = body['keys'];
-            for(var i = 0; i < keys.length; i++) {
-                //Convert each key to PEM
-                var key_id = keys[i].kid;
-                var modulus = keys[i].n;
-                var exponent = keys[i].e;
-                var key_type = keys[i].kty;
-                var jwk = { kty: key_type, n: modulus, e: exponent};
-                var pem = jwkToPem(jwk);
-                pems[key_id] = pem;
-            }
-            //validate the token
-            var decodedJwt = jwt.decode(token, {complete: true});
-            if (!decodedJwt) {
-                console.log("Not a valid JWT token");
-                return;
-            }
+function RegisterUser(userPoolId, clientId, userName, password, attributeList, onSucess, onError){
+    const userPool = createUserPoolData(userPoolId, clientId);
+    userPool.signUp(userName, password, attributeList, null, function(err, result){
+        if (err) {
+            onError(err);
 
-            var kid = decodedJwt.header.kid;
-            var pem = pems[kid];
-            if (!pem) {
-                console.log('Invalid token');
-                return;
-            }
-
-            jwt.verify(token, pem, function(err, payload) {
-                if(err) {
-                    console.log("Invalid Token.");
-                } else {
-                    console.log("Valid Token.");
-                    console.log(payload);
-                }
-            });
         } else {
-            console.log("Error! Unable to download JWKs");
+            onSucess(result.user);
         }
     });
 }
 
-Login();
-ValidateToken();
+function createUserPoolData(userPoolId, clientId) {
+    const poolData = {    
+        UserPoolId : userPoolId,
+        ClientId : clientId
+        }; 
+    
+    return new AmazonCognitoIdentity.CognitoUserPool(poolData);
+}
+
+function createCognitoUser(userPoolId, clientId, userName) {
+    const userPool = createUserPoolData(userPoolId, clientId);
+
+    const userData = {
+        Username : userName,
+        Pool : userPool
+    };
+    
+    return new AmazonCognitoIdentity.CognitoUser(userData);
+
+}
+
+function ConfirmUser(userPoolId, clientId, userName, code, onSuccess, onError){
+    var cognitoUser = createCognitoUser(userPoolId, clientId, userName);
+    cognitoUser.confirmRegistration(code, true, function(err, result) {
+        if (err) {
+            onError(err);
+        } else {
+            onSuccess(result);
+        }
+    });
+}
+
+function Login(userPoolId, clientId, userName, password, onSuccess, onError) {
+    var cognitoUser = createCognitoUser(userPoolId, clientId, userName);
+
+    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username : userName,
+        Password : password,
+    });
+    
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: onSuccess,
+        onFailure: onError  
+    });
+}
+
+
+var attributeList = [];
+createCognitoUserAttribute(attributeList, "name", config.name);
+createCognitoUserAttribute(attributeList, "birthdate", config.birthdate);
+createCognitoUserAttribute(attributeList, "email", config.email);
+createCognitoUserAttribute(attributeList, "phone_number", config.phone_number);
+
+RegisterUser(config.userPoolId, config.clientId, config.email, config.password, attributeList, 
+    function(cognitoUser) {
+        console.log('user name is ' + cognitoUser.getUsername());
+    },
+    function(err) {
+        console.log('Sing up error: ', err);
+    });
+
+ConfirmUser(config.userPoolId, config.clientId, config.email, config.verificationCode, 
+    function(result) {
+        console.log('Confirm user result:', result);
+    },
+    function(err) {
+        console.log('Confirm user error: ', err);
+    });
+
+Login(config.userPoolId, config.clientId, config.email, config.password,
+    function (result) {
+        console.log('access token + ' + result.getAccessToken().getJwtToken());
+        console.log('id token + ' + result.getIdToken().getJwtToken());
+        console.log('refresh token + ' + result.getRefreshToken().getToken());
+    },
+    function(err) {
+        console.log('Authentication error:', err);
+    });
