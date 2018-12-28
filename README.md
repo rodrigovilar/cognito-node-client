5fa3b8116d8e3106e88f29874b2a8cfbbeca1567
Rodrigo Vilar <rvilar@gmail.com>
2018-12-28 20:27:39 -0300

    Separating reusable code to lib/userpool.js, creating a config.json file and writing readme.

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # cognito-node-client
 A node client to authenticate into Amazon Cognito
+
+
+This project is based on Amazon's Tutorial: Integrating User Pools for JavaScript Apps https://docs.aws.amazon.com/cognito/latest/developerguide/tutorial-integrating-user-pools-javascript.html#tutorial-integrating-user-pools-confirm-users-javascript
+
+The goal is use Cognito User pools to sign up from a Node client.
+
+Usage steps:
+
+1. Create a User pool for your app
+  1. Sign in to the [Amazon Cognito console](https://console.aws.amazon.com/cognito/home)
+  2. Choose Manage your User Pools / Create a User Pool
+  3. Type pool name and choose Review defaults
+  4. From the left navigation pane, choose Attributes
+    After seting email, phone number and given name attributes as required and after users in the pool verifying their email addresses, they can sign in with their usernames or email addresses
+  5. Choose Next step until App clients
+  6. Choose Add an app client
+    1. Set App client name, uncheck Generate client secret and choose Create app client
+  7. Choose Next step until Review
+  8. Choose Create pool
+2. Note necessary ids
+  1. Note Pool Id
+  2. From the left navigation pane, choose App clients and note App client id
+3. Register user
+  1. Create a config.json file with the following structure:
+
+```javascript
+{
+    "userPoolId" : "us-east-1_XXXXXXXXX",
+    "clientId" : "xxxxxxxxxxxxxxxxxxxxxxxx",
+    "username" : "example",
+    "password" : "Passw0rd!",
+    "verificationCode" : "123456",
+    "attributes" : {
+        "given_name" : "Example user",
+        "email" :  "example@user.com",
+        "phone_number" : "+1234567890"
+    }
+}
+```
+
+  2. Invoke the RegisterUser function:
+
+```javascript
+const userpool = require('./lib/userpool');
+const config = require('./config.json');
+
+userpool.RegisterUser(config.userPoolId, config.clientId, config.username, config.password, config.attributes, 
+    function(cognitoUser) {
+        console.log('User name is ' + cognitoUser.getUsername());
+    },
+    function(err) {
+        console.log('Sing up error: ', err);
+    });
+```
+
+  3. The configured email (e.g. `example@user.com`) will receive the verification code that should be writen on config.json file
+  4. Invoke the ConfirmUser function:
+
+```javascript
+userpool.ConfirmUser(config.userPoolId, config.clientId, config.username, config.verificationCode, 
+    function(result) {
+        console.log('Confirm user result:', result);
+    },
+    function(err) {
+        console.log('Confirm user error: ', err);
+    });
+```
+
+  5. On Amazon Cognito console, open the User pool / General Settings / Users and Groups
+  6. Check the users status, should be `CONFIRMED` 
+
+4. Log in
+  1. Invoke the Login function
+
+```javascript
+userpool.Login(config.userPoolId, config.clientId, config.username, config.password,
+    function (result) {
+        console.log('access token + ' + result.getAccessToken().getJwtToken());
+        console.log('id token + ' + result.getIdToken().getJwtToken());
+        console.log('refresh token + ' + result.getRefreshToken().getToken());
+    },
+    function(err) {
+        console.log('Authentication error:', err);
+    });
+```
--- index.js
+++ index.js
@@ -1,88 +1,16 @@
-// This code is based on https://docs.aws.amazon.com/cognito/latest/developerguide/tutorial-integrating-user-pools-javascript.html#tutorial-integrating-user-pools-confirm-users-javascript
-
-const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
-global.fetch = require('node-fetch');
-
-var config = require('./config.json');
-
-function createCognitoUserAttribute(attributeList, name, value) {
-    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name: name, Value: value})); 
-}
-
-function RegisterUser(userPoolId, clientId, userName, password, attributeList, onSucess, onError){
-    const userPool = createUserPoolData(userPoolId, clientId);
-    userPool.signUp(userName, password, attributeList, null, function(err, result){
-        if (err) {
-            onError(err);
-
-        } else {
-            onSucess(result.user);
-        }
-    });
-}
-
-function createUserPoolData(userPoolId, clientId) {
-    const poolData = {    
-        UserPoolId : userPoolId,
-        ClientId : clientId
-        }; 
-    
-    return new AmazonCognitoIdentity.CognitoUserPool(poolData);
-}
-
-function createCognitoUser(userPoolId, clientId, userName) {
-    const userPool = createUserPoolData(userPoolId, clientId);
-
-    const userData = {
-        Username : userName,
-        Pool : userPool
-    };
-    
-    return new AmazonCognitoIdentity.CognitoUser(userData);
-
-}
-
-function ConfirmUser(userPoolId, clientId, userName, code, onSuccess, onError){
-    var cognitoUser = createCognitoUser(userPoolId, clientId, userName);
-    cognitoUser.confirmRegistration(code, true, function(err, result) {
-        if (err) {
-            onError(err);
-        } else {
-            onSuccess(result);
-        }
-    });
-}
-
-function Login(userPoolId, clientId, userName, password, onSuccess, onError) {
-    var cognitoUser = createCognitoUser(userPoolId, clientId, userName);
-
-    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-        Username : userName,
-        Password : password,
-    });
-    
-    cognitoUser.authenticateUser(authenticationDetails, {
-        onSuccess: onSuccess,
-        onFailure: onError  
-    });
-}
+const userpool = require('./lib/userpool');
+const config = require('./config.json');
 
 
-var attributeList = [];
-createCognitoUserAttribute(attributeList, "name", config.name);
-createCognitoUserAttribute(attributeList, "birthdate", config.birthdate);
-createCognitoUserAttribute(attributeList, "email", config.email);
-createCognitoUserAttribute(attributeList, "phone_number", config.phone_number);
-
-RegisterUser(config.userPoolId, config.clientId, config.email, config.password, attributeList, 
+userpool.RegisterUser(config.userPoolId, config.clientId, config.username, config.password, config.attributes, 
     function(cognitoUser) {
-        console.log('user name is ' + cognitoUser.getUsername());
+        console.log('User name is ' + cognitoUser.getUsername());
     },
     function(err) {
         console.log('Sing up error: ', err);
     });
 
-ConfirmUser(config.userPoolId, config.clientId, config.email, config.verificationCode, 
+userpool.ConfirmUser(config.userPoolId, config.clientId, config.username, config.verificationCode, 
     function(result) {
         console.log('Confirm user result:', result);
     },
@@ -90,7 +18,7 @@ ConfirmUser(config.userPoolId, config.clientId, config.email, config.verificatio
         console.log('Confirm user error: ', err);
     });
 
-Login(config.userPoolId, config.clientId, config.email, config.password,
+userpool.Login(config.userPoolId, config.clientId, config.username, config.password,
     function (result) {
         console.log('access token + ' + result.getAccessToken().getJwtToken());
         console.log('id token + ' + result.getIdToken().getJwtToken());
